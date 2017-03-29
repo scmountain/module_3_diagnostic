075d2c5bf56933e6eef94984562dabe62b6fff2d
Spencer Carter <scmountain@gmail.com>
2017-03-29 10:30:01 -0600

    nothign new to add, trying to find route to call in api to get anything.

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,6 +1,6 @@
 class NrelService
   def self.data(zip_code)
-    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV[client_key]}")
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['client_key']}")
     byebug
     JSON.parse(response.body)
   end
